ac8e9a2e8e4c1acad2cbba1286e8f5a38789c230
Omar Ayman <se.omarayman@gmail.com>
2023-01-31 14:01:48 +0200

    finished problem easy-tuple-to-object

--- playground/00011-easy-tuple-to-object.ts
+++ playground/00011-easy-tuple-to-object.ts
@@ -20,7 +20,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (number | string)[]> = {
+  [key in T[number]]: key
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
